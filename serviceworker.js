d8a2f1f2dda5e4c2da4f9f09c11ec415d59eae31
Malte Ihlefeld <malte@famihlefeld.de>
2024-02-26 01:26:04 +0100

    more debug

--- serviceworker.js
+++ serviceworker.js
@@ -32,7 +32,7 @@ const cacheFirst = async ({ request, fallbackUrl }) => {
         // When even the fallback response is not available,
         // there is nothing we can do, but we must always
         // return a Response object.
-        return new Response(`Network error, could not fulfill request ${request}`, {
+        return new Response(`Network error, could not fulfill request destination: ${request.destination} body: ${request.body}`, {
             status: 408,
             headers: { "Content-Type": "text/plain" },
         });
